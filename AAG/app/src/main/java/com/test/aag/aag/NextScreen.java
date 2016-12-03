6a0c23b97f4df1a2ab17bce22e0aa0515e3af72f
MadikaneRN <richardmadikane100@gmail.com>
2016-12-03 16:52:56 +0200

    Finish up app

---
+++ AAG/app/src/main/java/com/test/aag/aag/NextScreen.java
@@ -0,0 +1,13 @@
+package com.test.aag.aag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NextScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_screen);
+    }
+}
